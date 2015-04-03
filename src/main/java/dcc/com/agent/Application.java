a0986350363292fdbe4901b2ce834b06380400ad
tchambill <antony_epis@hotmail.com>
2015-04-02 20:29:27 -0400

    add most method

--- src/main/java/dcc/com/agent/Application.java
+++ src/main/java/dcc/com/agent/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
+@Scope("singleton")
 public class Application {
 
     public static void main(String[] args) throws Exception {
